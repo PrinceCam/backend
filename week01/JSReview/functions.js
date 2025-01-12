966e11d6ef74219ed516b258b1f693b94c07e87d
Cameron Jones <cjones.prsvr@gmail.com>
2025-01-12 14:24:27 -0600

    first

--- week01/JSReview/functions.js
+++ week01/JSReview/functions.js
@@ -1,35 +1,84 @@
-const returnEmptyString = function () {};
+const returnEmptyString = function () {
+    return ""
+};
+console.log(returnEmptyString())
 
-const returnZeroNumber = function () {};
+const returnZeroNumber = function () {
+    return 0
+};
+console.log(returnZeroNumber())
 
-const returnEmptyArray = function () {};
+const returnEmptyArray = function () {
+    return []
+};
+console.log(returnEmptyArray())
 
-const returnEmptyObject = function () {};
+const returnEmptyObject = function () {
+    let cam = {}
+    return cam
+};
+console.log(returnEmptyObject())
 
-const returnString = function () {};
+const returnString = function () {
+    return "I Love Coding"
+};
+console.log(returnString())
 
 const addition = function (num1, num2) {
-    console.log(num1, num2)
+   return num1 + num2
 
 };
 
-console.log(addition(2,4))
+console.log(addition(4,10))
 
-const subtraction = function () {};
+const subtraction = function (num1, num2) {
+    return num1 - num2
+};
 
-const multiplication = function () {};
+console.log(subtraction(4,10))
 
-const division = function () {};
+const multiplication = function (num1, num2) {
+    return num1 * num2
 
-const returnArray = function () {};
+};
+console.log(multiplication(4,10))
 
-const returnFirstIndex = function () {};
+const division = function (num1, num2) {
+    return num1 / num2
+};
+console.log(division(4,10))
 
-const returnSecondIndex = function () {};
+const returnArray = function () {
+    return ["Rams", "Cowgirls", "Steelers", "Ravens"]
+};
+console.log(returnArray([]))
 
-const returnArrayLength = function () {};
 
-const arraySum = function () {};
+const returnFirstIndex = function (array) {
+  return array[0];
+};
+console.log(returnFirstIndex([6, 12, 24]));
+
+
+const returnSecondIndex = function (array) {
+    return array[1]
+};
+console.log(returnSecondIndex([6, 12, 24]))
+
+
+const returnArrayLength = function (array) {
+    return array.length
+};
+console.log(returnArrayLength([6, 12, 18, 24]))
+
+const arraySum = function (array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum;
+};
+console.log(arraySum([6, 12, 18, 24]))
 
 const arraySubtraction = function () {};
 
